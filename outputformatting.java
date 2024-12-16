4fffe4c144b8d5cff1291883e9b1da559f594776
shaik aqsa fathima <shaikaqsa.78@gmail.com>
2024-12-16 20:27:24 +0530

    Add files via upload

---
+++ outputformatting.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+               Scanner scanner = new Scanner(System.in);
+        System.out.println("================================");
+        for (int i = 0; i < 3; i++) {
+            String str = scanner.next();
+            int num = scanner.nextInt();
+             System.out.printf("%-15s%03d\n", str, num);
+        }
+            System.out.println("================================");
+    }
+}
+
+
